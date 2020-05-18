2245b1aa45c1ca3c684441a4e3b62ac1a84fbbc4
Emily Resch <eresch527@gmail.com>
2020-05-18 15:01:33 -0500

    added workouts to heroku and fixed date display

--- models/workout.js
+++ models/workout.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const workoutSchema = new Schema({
     day: {
         type: Date,
-        required: true
+        default: Date.now
     },
     exercises: [
         {
